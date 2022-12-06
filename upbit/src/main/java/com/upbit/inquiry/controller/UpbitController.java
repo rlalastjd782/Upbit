59fdff89624ae25dd3139bc0e22021ebfa7ce497
rlalastjd782 <rlalastjd782@naver.com>
2022-12-06 16:21:41 +0900

    코인프로젝트 진행중 1206

--- upbit/src/main/java/com/upbit/inquiry/controller/UpbitController.java
+++ upbit/src/main/java/com/upbit/inquiry/controller/UpbitController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.upbit.inquiry.DTO.MainDTO;
+import com.upbit.inquiry.DTO.UpBitMarketAllDTO;
 import com.upbit.inquiry.service.MainService;
 import com.upbit.inquiry.service.Impl.UpbitServiceImpl;
 
@@ -46,6 +47,7 @@ public class UpbitController {
 		
 		@GetMapping(value = "/calendar")
 		public String calendarPage(String all,Model model) {
+			System.out.println(upbitService.getUpbitMarket(all));
 			model.addAttribute("model",upbitService.getUpbitMarket(all));
 			return "api/calendar";
 			
@@ -53,53 +55,8 @@ public class UpbitController {
 		
 		
 		@GetMapping(value = "/coinRate")
-		public String coinRatePage(String all,Model model) throws IOException, ParseException {
-			// 일자 없으면 빈값 지정 해서 사용
-			
-			System.out.println(upbitService.getUpbitMarket(all).get(0).getMarket());
-			String date =  "2022-12-05T01:00:00Z";
-			// 봉개수 지정
-			String count = "2";
-			// 마켓이름 <- 해당하는것은 리스트로 저장하여 for 문 돌려 리스트로 저장하기
-			
-			String marketName = "KRW-ETH";
-			
-			
-		 	// 1. URL을 만들기 위한 StringBuilder.
-	        StringBuilder urlBuilder = new StringBuilder("https://api.upbit.com/v1/candles/minutes/240?market="+marketName+"&to="+date+"&count="+count); /*URL*/
-	        // 3. URL 객체 생성.
-	        URL url = new URL(urlBuilder.toString());
-	        // 4. 요청하고자 하는 URL과 통신하기 위한 Connection 객체 생성.
-	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-	        // 5. 통신을 위한 메소드 SET.
-	        conn.setRequestMethod("GET");
-	        // 6. 통신을 위한 Content-type SET. 
-	        conn.setRequestProperty("Content-type", "application/json");
-	        // 7. 통신 응답 코드 확인.
-	        System.out.println("Response code: " + conn.getResponseCode());
-	        // 8. 전달받은 데이터를 BufferedReader 객체로 저장.
-	        BufferedReader rd;
-	        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-	            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-	        } else {
-	            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-	        }
-	        // 9. 저장된 데이터를 라인별로 읽어 StringBuilder 객체로 저장.
-	        StringBuilder sb = new StringBuilder();
-	        String line;
-	        while ((line = rd.readLine()) != null) {
-	            sb.append(line);
-	        }
-	        // 10. 객체 해제. 
-	        rd.close();
-	        conn.disconnect();
-	        // 11. 전달받은 데이터 확인.
-	        System.out.println(sb.toString());
-	        
-	        JSONParser parser = new JSONParser();
-	        JSONArray jsonArray = (JSONArray) parser.parse(sb.toString());
+		public String coinRatePage(String all,Model model) throws Exception{
 			model.addAttribute("model",upbitService.getUpbitMarket(all));
-			model.addAttribute("jsonArray", jsonArray) ;
 			return "api/coinRate";
 			
 		}
