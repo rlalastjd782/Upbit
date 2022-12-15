4b211e371975b37303d2e072c90cc94324fcb8a8
rlalastjd782 <rlalastjd782@naver.com>
2022-12-15 13:54:52 +0900

    매퍼 주석 변경

--- upbit/src/main/java/com/upbit/inquiry/mapper/CoinMapper.java
+++ upbit/src/main/java/com/upbit/inquiry/mapper/CoinMapper.java
@@ -13,9 +13,9 @@ public interface CoinMapper {
 	//랭킹가져오기
 	List<CoinResponse> rankingSelect() throws Exception;
 	
-	// 오른쪽 화면
+	//오른쪽 화면
 	List<CoinResponse> rankingSelect2(CoinRequest coin) throws Exception;
-	// 왼쪽화면
+	//왼쪽화면
 	List<CoinResponse> rankingSelect3(CoinRequest coin) throws Exception;
 
 	
